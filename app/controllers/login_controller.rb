578b5285b48b77442cd0d47d708e443881c94c7a
Gunnar Wolf <gwolf@gwolf.org>
2008-10-31 18:55:17 -0600

    Report success/failure on login

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,10 +4,16 @@ class LoginController < ApplicationController
   def login
     return true unless request.post?
     if @sysuser = Sysuser.ck_login(params[:login], params[:passwd])
+      flash[:notice] = _'Successfully logged in'
       session[:sysuser_id] = @sysuser.id
       redirect_to '/'
       return true
+    else
+      flash[:error] = _'Login failure'
     end
+  end
+
+  def edit
   end
 
   def logout
